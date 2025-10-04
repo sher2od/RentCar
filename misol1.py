8770a38b623ad7db6aa63a6bb57cbac014fd9b26
sher2od <sherzodqarshiboyev543@gmail.com>
2025-10-04 16:18:26 +0300

    car

---
+++ misol1.py
@@ -0,0 +1,47 @@
+# # TODO 1-masala
+
+# # def numbers(n):
+# #     total = 0
+# #     for i in range(1,n + 1):
+# #         total += i
+# #     return total
+
+# # n = int(input("son >> "))
+# # print("mana",numbers(n))
+
+
+
+# # TODO 2-masala polindrom
+
+# # def is_palindrome(word:str):
+# #     return word == word[::-1]
+
+# # soz = input("So‘z kiriting: ")
+# # if is_palindrome(soz):
+# #     print("Palindrom")
+# # else:
+# #     print("Palindrom emas")
+
+
+# # # TODO  3- masala faktoryal
+# # def faktoryal(n:int):
+# #     result = 1
+# #     for i in range(1,n+1):
+# #         result *= i
+# #     return result
+
+# # n = int(input("Son >> "))
+# # print(faktoryal(n))
+
+# # TODO 4-masala Eng katta sonni topish
+
+# def max_largest(nums:list):
+#     largest = nums[0]
+#     for num in nums:
+#         if num > largest:
+#             largest = num
+#     return largest
+
+# nums = [4,5,6,7,3,2,3,4]
+# print("Eng katta son",max_largest(nums))
+
